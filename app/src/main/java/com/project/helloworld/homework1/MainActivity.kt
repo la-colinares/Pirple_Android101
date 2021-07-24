fa577476e72c8865d15d8b185ce32b53c6f8267b
LA Colinares <dev.lacolinares@gmail.com>
2021-07-25 03:11:42 +0800

    Added Homework Assignment #2: Variable

--- app/src/main/java/com/project/helloworld/homework1/MainActivity.kt
+++ app/src/main/java/com/project/helloworld/homework1/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.project.helloworld
+package com.project.helloworld.homework1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.project.helloworld.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/project/helloworld/homework2/song.kt
@@ -0,0 +1,47 @@
+package com.project.helloworld.homework2
+
+
+/**
+ * Title: Homework Assignment #2: Variable
+ * From: Pirple/Nirmish
+ *
+ * Details: What's your favorite song?
+ */
+
+fun main(){
+    // String value of the song title
+    val title = "I Love You"
+    val artist = "Avril Lavigne"
+    val album = "Goodbye Lullaby"
+    // Integer value of released year
+    val yearReleased = 2011
+    val genre = "Pop"
+    val duration = "4:02"
+    // Beats per minute
+    val bpm = 107
+    val label = "RCA"
+    val engineers = "John Hanes and Michael Ilbert"
+    val writers = "Avril Lavigne, Max Martin, and Shellback"
+    val producers = "Max Martin and Shellback"
+    // International Standard Recording Code
+    val isrc = "USRC11100013"
+    val recordedAt = "Maratone Studios in Stockholm, Stockholms län, Sweden"
+    val mixedAt = "MixStar Studios in Virginia Beach, Virginia, United States"
+
+    println("\"Homework Assignment #2: Variable\"")
+    println("\"What's your favorite song?\"\n")
+    println("Song Title: $title")
+    println("Song Artist: $artist")
+    println("Album: $album")
+    println("Released Year: $yearReleased")
+    println("Genre: $genre")
+    println("Duration: $duration")
+    println("Beats per minute: $bpm")
+    println("Label: $label")
+    println("Engineers: $engineers")
+    println("Writers: $writers")
+    println("Producers: $producers")
+    println("ISRC: $isrc")
+    println("Recorded at: $recordedAt")
+    println("Mixed at: $mixedAt")
+}
